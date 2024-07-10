a36da2260195960fd1fcd761d68dd539a3382b75
shrikantg199 <shrikantg199@gmail.com>
2024-07-10 16:40:21 +0530

    added sign out

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,34 +1,21 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import { useAuth } from "@clerk/clerk-expo"; // Adjust according to your auth provider
+import { useAuth, useUser } from "@clerk/clerk-expo";
 
-const Profile = () => {
-  const navigation = useNavigation();
-  const { signOut } = useAuth(); // Adjust according to your auth provider
-
-  const handleLogout = async () => {
-    try {
-      await signOut();
-    } catch (err) {
-      console.error("Log out error", err);
-    }
-  };
-
+const profile = () => {
+  const { user } = useUser();
+  const { signOut } = useAuth();
+  console.log(user);
   return (
-    <View style={styles.container}>
-      <Text>this is my profile</Text>
-      <Button title="Log Out" onPress={handleLogout} />
+    <View>
+      <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      <Text>{user?.fullName}</Text>
+      <Button title="Sign Out" onPress={() => signOut()} />
+      {user?.imageUrl && <Image source={{ uri: user.imageUrl }} />}
     </View>
   );
 };
 
-export default Profile;
+export default profile;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+const styles = StyleSheet.create({});
