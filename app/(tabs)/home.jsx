1182c6c362727efabd12acac9c2d318f8dadeef8
shrikantg199 <shrikantg199@gmail.com>
2024-07-13 19:34:40 +0530

    initial commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,6 +2,8 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Header from "../../components/Home/Header";
 import Slider from "../../components/Home/Slider";
+import Category_List from "../../components/Home/Category_List";
+import BusinessList from "../../components/Home/BusinessList";
 
 const Home = () => {
   return (
@@ -9,10 +11,12 @@ const Home = () => {
       <View>
         {/* headers */}
         <Header />
-        <Slider />
         {/* slider */}
+        <Slider />
         {/* category */}
+        <Category_List />
         {/* business List */}
+        <BusinessList />
       </View>
     </View>
   );
