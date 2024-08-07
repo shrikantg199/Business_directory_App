45b1980605ff7d1277af2323acf2f811aaa366b3
shrikantg199 <shrikantg199@gmail.com>
2024-08-07 19:57:49 +0530

    initial commit

--- components/LogInScreen.jsx
+++ components/LogInScreen.jsx
@@ -8,8 +8,6 @@ import { colors } from "@/constants/Colors";
 
 export const useWarmUpBrowser = () => {
   React.useEffect(() => {
-    // Warm up the android browser to improve UX
-    // https://docs.expo.dev/guides/authentication/#improving-user-experience
     void WebBrowser.warmUpAsync();
     return () => {
       void WebBrowser.coolDownAsync();
